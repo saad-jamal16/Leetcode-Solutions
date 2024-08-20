5b26986d79e197309ec3008970af7ccd1814e6dd
Saad jamal <42598761+saad-jamal16@users.noreply.github.com>
2024-08-20 20:13:28 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (40.43%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -1,14 +1,14 @@
 class Solution {
     public String defangIPaddr(String address) {
-        StringBuilder str = new StringBuilder();
-        for (int i = 0; i < address.length(); i++){
-            if (address.charAt(i) == '.'){
-                str.append("[.]");
-            } else {
-                str.append(address.charAt(i));
-            }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < address.length(); i++) {
+            if (address.charAt(i) == '.')
+                sb.append("[.]");
+            else
+                sb.append(address.charAt(i));
         }
-        return str.toString();
+        
+        return sb.toString();
         
     }
 }
