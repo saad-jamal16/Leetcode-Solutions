da4f590b506552fbec223615b8ab9d05b7e7eaa9
Saad jamal <42598761+saad-jamal16@users.noreply.github.com>
2024-09-30 11:40:15 +0530

    Time: 0 ms (100.00%), Space: 44 MB (59.33%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -10,28 +10,23 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        // Check if the head is null or if the list has only one node
-        if (head == null || head.next == null) {
+        if(head == null || head.next == null){
             return head;
         }
         ListNode prev = head;
-        ListNode temp = prev.next;
-        // two pointer has been declared one prev will see head and move one node at a time
-        // while temp will try to match the node with the first node.
-        
+        ListNode temp = head.next;
         while(temp != null){
             if(temp.val == prev.val){
                 temp = temp.next;
                 continue;
             }
-            // if the temp and prev node will be matched it will skip the node
             prev.next = temp;
             prev = temp;
             temp = temp.next;
         }
-        // and then it will return the head as the new sorted linkedlist.
         prev.next = null;
         return head;
+        
         
     }
 }
