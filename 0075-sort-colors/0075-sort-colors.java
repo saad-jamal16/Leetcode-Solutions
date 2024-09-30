c37d06756d293f00b74103e612dde4450ff3c39f
Saad jamal <42598761+saad-jamal16@users.noreply.github.com>
2024-09-30 11:01:48 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (48.09%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,30 +1,28 @@
 class Solution {
     public void sortColors(int[] nums) {
         int red = 0;
-        int white = 0;  // declared all the colors as 0 so that i can use them for storing arrays
+        int white = 0;
         int blue = 0;
-        
-        for(int i = 0; i < nums.length; i++){  
+        for(int i = 0; i < nums.length; i++){
             if(nums[i] == 0){
-                red++;           //if nums element  will be zero red conunter will be incremented
-            } else if (nums[i] == 1){
+                red++;
+            }else if(nums[i] == 1){
                 white++;
-            } else{
-                blue++; //After this loop, the variables red, white, and blue will hold the counts of 0s, 1s, and 2s
+            }else{
+                blue++;
             }
         }
-        // this loop is iterated to sort the arrays:
-        
+        // to sort the colours:
         for(int j = 0; j < nums.length; j++){
-            if(red > 0){     // if red is greater then 0 then there are still 0's left
+            if(red  > 0){
                 nums[j] = 0;
-                red--;        // so if there it will bring it in sorted form.
-            } else if( white > 0){
+                red--;
+            }else if( white > 0){
                 nums[j] = 1;
                 white--;
-            } else{
+            }else{
                 nums[j] = 2;
-                blue--;
+                    blue--;
             }
         }
         
