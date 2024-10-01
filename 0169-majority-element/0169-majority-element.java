c31671ec58edd4e6f5e68adc7d6823f331d256a4
Saad jamal <42598761+saad-jamal16@users.noreply.github.com>
2024-10-01 16:05:14 +0530

    Time: 4 ms (54.02%), Space: 53.2 MB (22.13%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,8 +1,8 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);  // when array is sorted it will come in middle position
-        int n = nums.length;  //size of array
-        return nums[n/2];    //and then the middle position is divide by size of array
+        Arrays.sort(nums);
+        int n = nums.length;
+        return nums[n/2];
+        
+    }
 }
-}
-    
