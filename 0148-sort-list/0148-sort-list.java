6878f3b3e9ab9725de2e813bfcf2bb259fe999ba
Saad jamal <42598761+saad-jamal16@users.noreply.github.com>
2024-10-15 14:11:21 +0530

    Time: 8 ms (99.10%), Space: 55.8 MB (75.79%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -10,12 +10,14 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
+        // inialized count to store values and temp as start node
         int count = 0;
         ListNode temp = head;
         while(temp != null){
             count++;
             temp = temp.next;
         }
+        // inialized arrays to store all elements in arrays so that sorting becomes easy
         int []arr = new int[count];
         count = 0;
         temp = head;
@@ -23,6 +25,7 @@ class Solution {
             arr[count++] = temp.val;
             temp = temp.next;
         }
+        // direcly sorted the array
         Arrays.sort(arr);
         temp = head;
         count = 0;
