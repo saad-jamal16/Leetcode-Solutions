5e58cca7543b36191a52d3983c23a2e7bf2896a1
Saad jamal <42598761+saad-jamal16@users.noreply.github.com>
2024-10-20 16:18:27 +0530

    Time: 1 ms (98.88%), Space: 41.3 MB (7.30%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        long start = 0;
+        long end = n;
+        while(start <= end){
+            long mid = (start + end)/2;
+            long sum = mid*(mid+1)/2;
+            if(sum == n){
+                return (int)mid;
+            }else if (sum > n){
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
+        }
+        return (int)end;
+        
+    }
+}
