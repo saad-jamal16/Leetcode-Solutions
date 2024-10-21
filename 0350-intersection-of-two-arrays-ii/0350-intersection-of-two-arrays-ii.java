e0471fba53475d11b975f183abb4e77eacfa333d
Saad jamal <42598761+saad-jamal16@users.noreply.github.com>
2024-10-21 12:24:22 +0530

    Time: 5 ms (23.79%), Space: 45.1 MB (5.19%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -2,33 +2,50 @@ import java.util.*;
 
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);  // Sort both arrays
-        Arrays.sort(nums2);
+        Arrays.sort(nums2);  // Sort nums2 for binary search
         
-        int n = nums1.length;
-        int m = nums2.length;
-        List<Integer> resultList = new ArrayList<>(); // Using a list to collect the result dynamically
-        
-        int i = 0, j = 0;  // Two pointers
-        
-        while (i < n && j < m) {  // Traverse both arrays using two pointers
-            if (nums1[i] == nums2[j]) {  // If elements are the same, add to result
-                resultList.add(nums1[i]);
-                i++;
-                j++;
-            } else if (nums1[i] < nums2[j]) {  // Move pointer of the smaller element
-                i++;
-            } else {
-                j++;
+        List<Integer> resultList = new ArrayList<>();
+        for (int num : nums1) {
+            int index = binarySearch(nums2, num);  // Perform binary search on nums2 for each num in nums1
+            if (index != -1) {  // If element is found
+                resultList.add(num);  // Add it to the result
+                nums2 = removeElement(nums2, index);  // Remove the found element from nums2
             }
         }
         
         // Convert the result list to an array
         int[] result = new int[resultList.size()];
-        for (int k = 0; k < resultList.size(); k++) {
-            result[k] = resultList.get(k);
+        for (int i = 0; i < resultList.size(); i++) {
+            result[i] = resultList.get(i);
         }
         
-        return result;  // Return the intersection array
+        return result;
+    }
+    
+    // Binary search function
+    private int binarySearch(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;  // Return the index where the element was found
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;  // Return -1 if the element is not found
+    }
+    
+    // Function to remove element at a specific index from an array
+    private int[] removeElement(int[] nums, int index) {
+        int[] newArr = new int[nums.length - 1];
+        for (int i = 0, j = 0; i < nums.length; i++) {
+            if (i != index) {
+                newArr[j++] = nums[i];
+            }
+        }
+        return newArr;
     }
 }
