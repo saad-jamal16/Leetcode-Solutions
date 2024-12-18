4dbcaee6acd18235b29c0734db11db302282982a
Saad jamal <42598761+saad-jamal16@users.noreply.github.com>
2024-12-18 11:51:00 +0530

    Time: 377 ms (5.03%), Space: 45.9 MB (16.39%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -3,11 +3,10 @@ class Solution {
         for(int i = 0; i < nums.length; i++){
             int leftsum = 0;
             int rightsum = 0;
-            
             for(int j = 0; j < i; j++){
                 leftsum += nums[j];
             }
-            for(int j = i+1; j<nums.length; j++){
+            for(int j = i + 1; j < nums.length; j++){
                 rightsum += nums[j];
             }
             if(leftsum == rightsum){
