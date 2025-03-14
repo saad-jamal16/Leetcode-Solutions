bd3359e92fcb56f98c0632b8f3adc8e81c8464be
Saad jamal <42598761+saad-jamal16@users.noreply.github.com>
2025-03-14 16:41:33 +0530

    Time: 0 ms (100%), Space: 43.3 MB (27.58%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,20 +1,20 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-           int left = 0, right = nums.length - 1;
+         int left = 0;
+        int right = nums.length - 1;
+
         while (left <= right) {
             int mid = left + (right - left) / 2;
+
             if (nums[mid] == target) {
-                return mid;   // if found, return index
-            }
-            if (target < nums[mid]) {
+                return mid;
+            } else if (nums[mid] > target) {
                 right = mid - 1;
             } else {
                 left = mid + 1;
             }
         }
 
-        // if not found, the insert position will be retained by 'lo'
-        // WHY and HOW? Watch my video!
-        return left;  
+        return left; 
     }
 }
