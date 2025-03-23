ca4f4f50569146594ce7606609b915f3791e0205
Saad jamal <42598761+saad-jamal16@users.noreply.github.com>
2025-03-23 14:46:27 +0530

    Time: 3 ms (89.82%), Space: 45.1 MB (62.9%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,38 +1,15 @@
 class Solution {
     public String convert(String s, int numRows) {
-        if(numRows==1 || numRows >=s.length())
-         {
-            return s;
-         }
-        StringBuilder[] rows = new StringBuilder[numRows];
-
-        for(int i=0;i<numRows;i++)
-        {
-            rows[i] = new StringBuilder();
+        if (numRows == 1) return s;
+        StringBuilder a = new StringBuilder();
+        for (int i = 0; i < numRows; i++) {
+            for (int j = i; j < s.length(); j += (2 * (numRows - 1))) {
+                a.append(s.charAt(j));
+                if (i > 0 && i < numRows - 1 && j + (2 * (numRows - 1)) - (2 * i) < s.length()) {
+                    a.append(s.charAt(j + (2 * (numRows - 1)) - (2 * i)));
+                }
+            }
         }
-
-        int currentRow = 0;
-        boolean goingDown = false;
-
-        for(int i=0;i<s.length();i++)
-        {
-            char c = s.charAt(i);
-          
-           rows[currentRow].append(c);
-           if(currentRow==0 ||currentRow==numRows-1)
-           {
-            goingDown = !goingDown;
-           }
-
-           currentRow += goingDown ? 1:-1;
-
-        }
-       StringBuilder result = new StringBuilder();
-       for(int i=0;i<rows.length;i++)
-       {
-        result.append(rows[i]);
-       }
-      return result.toString();
-
+        return a.toString(); 
     }
 }
