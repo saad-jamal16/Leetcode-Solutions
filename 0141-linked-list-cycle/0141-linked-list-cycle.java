00018b7955982c8b681f80b439397e1cb5adbb44
Saad jamal <42598761+saad-jamal16@users.noreply.github.com>
2025-03-28 19:11:00 +0530

    Time: 0 ms (100%), Space: 44.8 MB (18.81%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,20 +11,18 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        // same two pointers algorithm was used here 
-        // declared two listNode both from head one will move 
-        //one node while other will move two nodes at a time
+                ListNode fast = head;
         ListNode slow = head;
-        ListNode fast = head;
-        //both pointer will move from head to tail
-        while(slow != null && fast != null && fast.next != null){
-            slow = slow.next;
+
+        while (fast != null && fast.next != null) {
             fast = fast.next.next;
-            // if both the pointer will meet at same node that means it has cycle
-            if(slow == fast){
+            slow = slow.next;
+
+            if (fast == slow) {
                 return true;
             }
         }
-        return false;
+
+        return false; 
     }
 }
