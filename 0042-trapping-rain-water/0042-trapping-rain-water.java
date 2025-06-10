15e3d65fedb3adf59e5c3b9e8539945cfd9d2062
Saad jamal <42598761+saad-jamal16@users.noreply.github.com>
2025-06-10 14:52:05 +0530

    Time: 1 ms (62.26%), Space: 47 MB (8.64%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,32 +1,21 @@
 class Solution {
     public int trap(int[] height) {
-        
-        int n = height.length;
-        if (n == 0) return 0;
-        
-        int[] left = new int[n];
-        int[] right = new int[n];
-        int storedWater = 0;
-        
-        // Fill left array
-        left[0] = height[0];
-        for (int i = 1; i < n; i++) {
-            left[i] = Math.max(left[i - 1], height[i]);
+        int ans = 0;
+        int l = 0;
+        int r = height.length-1;
+        int lmax = 0, rmax = 0;
+        while(l < r){
+            lmax = Math.max(lmax,height[l]);
+            rmax =  Math.max(rmax,height[r]);
+            if(lmax < rmax){
+                ans += lmax - height[l];
+                l++;
+            }else{
+                ans += rmax - height[r];
+                r--;
+
+            }
         }
-        
-        // Fill right array
-        right[n - 1] = height[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            right[i] = Math.max(right[i + 1], height[i]);
-        }
-        
-        // Calculate trapped water
-        for (int i = 0; i < n; i++) {
-            int minHeight = Math.min(left[i], right[i]);
-            storedWater += minHeight - height[i];
-        }
-        
-        return storedWater;
- 
+        return ans;
     }
 }
