00c3e92909c24e84eef63a7c6315f62aa39ac39b
Saad jamal <42598761+saad-jamal16@users.noreply.github.com>
2025-06-26 15:03:53 +0530

    Time: 6 ms (72.4%), Space: 44.7 MB (67.33%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,30 +1,35 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack <Integer> st = new Stack<>();
-        String operators ="+-*/";
-        
-        for(String token : tokens){
-            // if index of token is not equal to -1 that means token is operator:
-            if(operators.indexOf(token) != -1){
-                int d2 = st.pop();
-                int d1 = st.pop();
-                int res = 0;
-            
-            if(token.equals("+"))
-                res = d1+d2;
-            else if(token.equals("-"))
-                res = d1-d2;
-            else if(token.equals("*"))
-                res = d1*d2;
-            else if(token.equals("/"))
-                res = d1/d2;
-            st.push(res);
+        Stack<Integer> st = new Stack<>();
+        int result = 0;
+
+        for (String s : tokens) {
+            if (s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/")) {
+                int b = st.pop();
+                int a = st.pop();
+
+                result = operate(a, b, s);
+                st.push(result);
+            } else {
+                st.push(Integer.parseInt(s));
+            }
         }
-        else
-            // to convert string into integer
-            st.push(Integer.parseInt(token));
+
+        return st.pop();
     }
-        return st.peek();
-        
+        public int operate(int a, int b, String s) {
+        if (s.equals("+"))
+            return a + b;
+
+        if (s.equals("-"))
+            return a - b;
+
+        if (s.equals("*"))
+            return (int) ((long) a * (long) b);
+
+        if (s.equals("/"))
+            return a / b;
+
+        return -1;
     }
 }
