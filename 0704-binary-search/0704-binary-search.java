af2ebb8fdb643f4f17cc27a2962baaec0d399164
Saad jamal <42598761+saad-jamal16@users.noreply.github.com>
2025-07-20 16:56:01 +0530

    Time: 1 ms (10.2%), Space: 46.3 MB (7.36%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,11 +1,10 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int n = nums.length;
-        for(int i = 0; i < n; i++){
-            if(nums[i] == target)
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == target){
                 return i;
+            }
         }
         return -1;
-        
     }
 }
