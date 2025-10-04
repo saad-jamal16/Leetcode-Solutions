96ac1cb9be3da3236e822a20776ab39e0dbb51fd
Saad jamal <42598761+saad-jamal16@users.noreply.github.com>
2025-10-04 15:35:40 +0530

    Time: 5 ms (77.27%), Space: 57.9 MB (56.57%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,22 +1,18 @@
 class Solution {
     public int maxArea(int[] height) {
-        int maxwater = 0;
-        // lp = leftPointer, rp = rightPointer
-        int lp = 0, rp = height.length-1;
-        while(lp < rp){
-            int width = rp - lp;
-            int ht = Math.min(height[lp],height[rp]);
-            //current water will store
-            int curwater = width * ht;
-            maxwater = Math.max(maxwater,curwater);
-            // minimum height pointer will be ++,--;
-            if(height[lp] < height[rp]){
-                lp++;
-            }else{
-                rp--;
+        int maxArea = 0;
+        int i = 0, j = height.length - 1;
+
+        while (i < j) {
+            int currArea = Math.min(height[i], height[j]) * (j - i);
+            maxArea = Math.max(maxArea, currArea);
+
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
             }
         }
-        return maxwater;
-        
+        return maxArea;
     }
 }
