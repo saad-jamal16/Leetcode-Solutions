09ce4082312994db690c8db9179fd455c29e9de2
Saad jamal <42598761+saad-jamal16@users.noreply.github.com>
2025-10-28 19:05:17 +0530

    Time: 0 ms (100%), Space: 42.3 MB (48.19%) - LeetHub

--- 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.java
+++ 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.java
@@ -1,32 +1,22 @@
 class Solution {
     public int countValidSelections(int[] nums) {
-         int n = nums.length;
-        int selections = 0;
+        int count = 0, n = nums.length;
+        int sum = 0, halfSum = 0;
 
-        int[] prefixSum = new int[n];
-        int[] suffixSum = new int[n];
+        for (int num : nums) sum += num;
 
-        // Build prefix sums
-        for (int i = 1; i < n; ++i)
-            prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
-
-        // Build suffix sums
-        for (int i = n - 2; i >= 0; --i)
-            suffixSum[i] = suffixSum[i + 1] + nums[i + 1];
-
-        for (int i = 0; i < n; ++i) {
-            if (nums[i] != 0)
-                continue;
-
-            // Both sides equal zero at the same time
-            if (prefixSum[i] == suffixSum[i])
-                selections += 2;
-
-            // Only one side equals zero at the end
-            if (Math.abs(prefixSum[i] - suffixSum[i]) == 1)
-                selections += 1;
+        for (int i = 0; i < n; i++) {
+            halfSum += nums[i];
+            if (nums[i] == 0) {
+                if (2 * halfSum == sum) {
+                    count += 2;
+                } else if (Math.abs(sum - 2 * halfSum) == 1) {
+                    count++;
+                } 
+            }
         }
+        
+        return count;
 
-        return selections;
     }
 }
